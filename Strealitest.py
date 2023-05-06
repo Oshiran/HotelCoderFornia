8861576b558b14538cc7a4d03efc1c6fbacd16ae
Alison Dwyne <102777652@students.swinburne.edu.my>
2023-05-07 00:04:21 +0800

    fixing files per specs

---
+++ Strealitest.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+df= pd.DataFrame({ 'first column': [1,2,3,4], 'second column':[10,20,30,40]})
+
+
+option = st.selectbox('which number do you like best?',
+df['first column'])
+
+'you selected:', option
