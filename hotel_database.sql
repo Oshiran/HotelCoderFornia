99b064d77fbae24e9ee8876041b3c90f8971cab1
Alison Dwyne <130299295+Oshiran@users.noreply.github.com>
2023-05-06 23:42:13 +0800

    Add files via upload
    Initial files

---
+++ hotel_database.sql
@@ -0,0 +1,58 @@
+-- Create a Schema First
+CREATE Schema if NOT EXISTS hotel_database;
+
+-- Initialize the schema
+USE hotel_database;
+
+-- Customer Table
+CREATE TABLE if NOT EXISTS Customers(
+    ID int AUTO_INCREMENT PRIMARY KEY,
+    L_Name varchar(255) not null,
+    F_Name varchar(255) not null,
+    Passport_No varchar(255) not null UNIQUE,
+    Passport_Exp date not null,
+    DOB date not null,
+    Phone_no int(11) not null UNIQUE,
+    Nationality varchar(255) not null,
+    Sex varchar(10) check (Sex in ('Female','Male')),
+    Email varchar(300) not null UNIQUE,
+    Credit_Card varbinary(256) UNIQUE
+);
+
+-- Hotel Room
+Create TABLE if NOT EXISTS Room(
+    Hotel_ID int AUTO_INCREMENT PRIMARY KEY,
+    Beds int(255) not null,
+    Bed_Type varchar(50) check (Bed_Type in ('Single','Double','Twin','Queen','King')),
+    Price int(255) not null,
+    Minibar BOOLEAN not null,
+    Entertainment_system BOOLEAN not null,
+    BathTub BOOLEAN not null,
+    Notes varchar(255) DEFAULT '-'
+);
+
+-- Booking
+Create TABLE if NOT EXISTS Booking(
+    Booking_ID int AUTO_INCREMENT PRIMARY KEY NOT null,
+    FOREIGN KEY(ID) REFERENCES Customers(ID),
+    FOREIGN KEY(Hotel_ID) REFERENCES Room(Hotel_ID),
+    Arrival_Date date not null,
+    Departure_Date date not null,
+    Pax int(255) not null 
+);
+
+--Guest
+Create Table if NOT EXISTS Guest(
+    Guest_ID int AUTO_INCREMENT PRIMARY KEY NOT null,
+    ID int not null,
+    FOREIGN KEY (ID) REFERENCES Customers(ID),
+    Hotel_ID int not null,
+    FOREIGN KEY (Hotel_ID) REFERENCES Room(Hotel_ID),
+    Booking_ID int not null,
+    FOREIGN Key (Booking_ID) REFERENCES Booking(Booking_ID),
+    Guest_F_Name varchar(255) not null,
+    Guest_L_Name varchar(255) not null,
+    Guest_Passport varchar (255) not null,
+    G_Notes varchar (255) DEFAULT '-'
+);
+
---
+++ main.py
@@ -0,0 +1,23 @@
+import pymysql
+
+# Establish a connection to the SQL Server database
+conn = pymysql.connect(db='sample', user='root', passwd='', host='localhost', port=3306)
+
+# Create a cursor object to interact with the database
+# cursor = conn.cursor()
+
+# # Execute a query
+# cursor.execute('SELECT * FROM your_table')
+
+# # Fetch the results of the query
+# results = cursor.fetchall()
+
+# # Print the results
+# for row in results:
+#     print(row)
+
+# Close the cursor and connection
+# cursor.close()
+
+print(conn)
+conn.close()
